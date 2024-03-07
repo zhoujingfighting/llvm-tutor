4cf0eff6842ab897845c3ee5ba0455d9d7a2846a
zhoujing <jing.zhou@terapines.com>
2024-03-07 09:36:32 +0800

    Mem2Reg initial commit

---
+++ Mem2Reg/Mem2Reg.cpp
@@ -0,0 +1,77 @@
+//=============================================================================
+// FILE:
+//    HelloWorld.cpp
+//
+// DESCRIPTION:
+//    Mem2Reg pass : memory to register pass, eliminate memory to register,
+//    especially alloca, load, store instructions
+//
+// USAGE:
+//    New PM
+//      opt -load-pass-plugin=libHelloWorld.dylib -passes="hello-world" `\`
+//        -disable-output <input-llvm-file>
+//
+//
+// License: MIT
+//=============================================================================
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+//-----------------------------------------------------------------------------
+// HelloWorld implementation
+//-----------------------------------------------------------------------------
+// No need to expose the internals of the pass to the outside world - keep
+// everything in an anonymous namespace.
+namespace {
+
+// This method implements what the pass does
+void visitor(Function &F) {
+    errs() << "(Mem2Reg) Hello from: "<< F.getName() << "\n";
+    errs() << "(Mem2Reg) number of arguments: " << F.arg_size() << "\n";
+}
+
+// New PM implementation
+struct Mem2Reg : PassInfoMixin<Mem2Reg> {
+  // Main entry point, takes IR unit to run the pass on (&F) and the
+  // corresponding pass manager (to be queried if need be)
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+    visitor(F);
+    return PreservedAnalyses::all();
+  }
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone.
+  static bool isRequired() { return true; }
+};
+} // namespace
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getMem2RegInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "mem2reg1") {
+                    FPM.addPass(Mem2Reg());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize HelloWorld when added to the pass pipeline on the
+// command line, i.e. via '-passes=hello-world'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getMem2RegInfo();
+}
